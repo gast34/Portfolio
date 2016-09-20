87ff33e81cfae2549fbae7e0545cfda2b73a5782
gaston <saboy.gaston@gmail.com>
2016-09-20 14:47:27 +0200

    Portfolio quasiment terminé

--- src/SiteBundle/Controller/AdvertController.php
+++ src/SiteBundle/Controller/AdvertController.php
@@ -5,19 +5,36 @@ namespace SiteBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
 
 class AdvertController extends Controller {
 
     /**
      * 
-     * @Route("/")
+     * @Route("/" , name = "home")
      * @Template("index.html.twig")
      */
-    public function getIndex() {
-
+    public function getIndex(Request $request) {
         
-
+         $message = \Swift_Message::newInstance();
+        $message->setSubject("Objet");
+        $message->setFrom('saboy.gaston34@gmail.com');
+        $message->setTo('kayye34@gmail.com');
+        // pour envoyer le message en HTML
+        $message->setBody('Hello world');
+        // pour envoyer le message en HTML
+        $message->setBody('<p>Hello world</p>','text/html'); 
+        //envoi du message
+         $this->get('mailer')->send($message);
+/*
+        $email = $request->request->get('mail');
+        $obj = $request->request->get('objet');
+        $corps = $request->request->get('corp');
+      */
+        
+        
     }
 
 }
